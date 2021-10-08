938e57f8e0d007feaa4070c6c0126472e2fe48b9
BurinSrivilai <43096170+BurinSrivilai@users.noreply.github.com>
2021-10-08 09:16:38 +0700

    slide

---
+++ app/src/main/java/burin/srivilai/ttpcargo/itdev/co/image_slide/SliderItem.java
@@ -0,0 +1,15 @@
+package burin.srivilai.ttpcargo.itdev.co.image_slide;
+
+public class SliderItem {
+    private  int images;
+
+    SliderItem(int image){
+        this.images = image;
+    }
+
+    public int getImage(){
+        return images;
+    }
+
+
+}
